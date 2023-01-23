4bc1199ed312583aefe9db31bcbe7c4eb17ecf21
zim <webmaster@coolguy.website>
2023-01-24 09:58:13 +1300

    update repo path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     publicPath:
         process.env.NODE_ENV === "production"
-            ? "/ii/android-webinstall"
+            ? "android-webinstall"
             : "/",
     transpileDependencies: ["vuetify"],
     chainWebpack: (config) => {
